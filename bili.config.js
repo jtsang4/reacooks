ed53aef0c8a67ff1f3d1746c81f0adf410ae2719
wtzeng1 <wentao_zeng1@163.com>
2019-08-29 14:16:31 +0800

    setup scaffold

---
+++ bili.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  input: './src/index.ts',
+  output: {
+    dir: './dist',
+    format: ['cjs', 'esm'],
+    minify: false,
+    moduleName: 'reacooks'
+  },
+  plugins: {
+    typescript2: {
+      cacheRoot: path.join(__dirname, '.rpt2_cache'),
+    },
+  },
+};
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rootDir: process.cwd(),
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+};
